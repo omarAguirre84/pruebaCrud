b8ccea96762fb63a679efafdfc32faeabdac3087
Omar Aguirre <omar.aguirre.1984@gmail.com>
2017-01-06 11:22:12 -0300

    pocos cambios

--- src/pruebaCRUD/testConexion.java
+++ src/pruebaCRUD/testConexion.java
@@ -3,15 +3,12 @@ package pruebaCRUD;
 import java.util.ArrayList;
 
 public class testConexion {
-	
-	
 	public static void main(String[] args) {
 		
 		String dbURL = "jdbc:mysql://192.168.1.2:3306/ANISDB";
 		String username = "omar";
 		String password = "creat96";
 		String sql = new Query("select * from anis").getQuery();
-
 	
 		ConexionABase c = new ConexionABase();
 		
@@ -19,8 +16,6 @@ public class testConexion {
 		ArrayList<Row> recoleccion = RecolectarDatos.recolectar(c.ejecutarQuery(sql));
 		
 		EscribirArchivo.escribir(recoleccion);
-		c.cerrarConexion();
-		
+		c.cerrarConexion();	
 	}
-
 }
